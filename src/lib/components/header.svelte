3c0ca6459e55208c7bd928d2f4d6603e5f1e7c55
Ray Wofford <raywofforddesign@gmail.com>
2024-02-19 01:26:58 -0600

    added shy header as part of main header component

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,15 +1,46 @@
 
 <style>
 	header{
-		width:100%;
-		height: 65ox;
+		width:100vw;
+		height: 65px;
 		background: rgb(212,230,233);
 		background: linear-gradient(180deg, rgba(230,233,233,1) 0%, rgba(240,245,245,1) 33%, rgba(252,252,252,1) 75%, rgba(245,245,245,1) 100%);
 		border-bottom: 2px solid rgba(244,244,244,1);
 		border-bottom-right-radius: 10px;
 		border-bottom-left-radius: 10px;
+	}
+
+	.unhide{		
+		height:80px;
+		z-index: 100;
+		position: fixed;
+		translate: 0 -55px;
+		transition: all 1s ease-in-out;
+	}
+
+	.unhide:hover{
+		translate: 0;
 
 	}
+	.unhide:hover > .hide{
+		display:inline-block;
+		margin-top: 0;
+		translate: 0;
+		transition: all 1s ease-in-out;
+		
+	}
+
+	.hide{		
+		transition: all 0.5s ease-in-out;
+		translate: 0 -80px;
+		z-index: 100;
+		
+	}
+
+	.gone{
+		display: none;
+	}
+
 	.flex{
 		display:flex;
 		justify-content: space-between;
@@ -61,8 +92,13 @@
 </style>
 <script>
 	import logo from "$lib/images/logo-ph.png";
+	let y = 0
 </script>
 
+<svelte:window bind:scrollY='{y}' />
+<!-- this is the shy header, reveals on hover and disappears when main header enters view -->
+<div class="unhide">
+<div class:gone={y<60} class="hide" >
 	<header class="flex"> 
 		<div class="flex flex-item flex-center" style="justify-content: flex-start;">
 			<a href="/" class="nostyle">
@@ -74,12 +110,37 @@
 		</div>
 		<!--blank div for spacing -->
 		<div class="flex-item"></div>
-		<ul class="flex flex-item flex-center" style="padding:5px; justify-content: space-between;">
-		
-			<div><h1><a href="/about">About</a></h1></div>
-			<div><h1><a href="/contact">Contact</a></h1></div>
-			<div><h1><a href="/projects">Projects</a></h1></div>
-			<div><h1><a href="/resume">Resume</a></h1></div>
-			<div><!--another blank div for spacing (these are really useful in flexbox)--></div>
-		</ul>
+		<nav class="flex-item" style = "padding-right: 1vw;">
+			<ul class="flex flex-item flex-center" style="padding:5px; justify-content: space-between;">
+			
+				<div><h1><a href="/about">About</a></h1></div>
+				<div><h1><a href="/contact">Contact</a></h1></div>
+				<div><h1><a href="/projects">Projects</a></h1></div>
+				<div><h1><a href="/resume">Resume</a></h1></div>
+			</ul>
+		</nav>
+	</header>
+</div>
+</div>
+<!-- main header-->
+	<header class="flex"> 
+		<div class="flex flex-item flex-center" style="justify-content: flex-start;">
+			<a href="/" class="nostyle">
+				<img src={logo} 
+				style="padding: 5px;"
+				width=36px height=36px>
+				</a>
+			<a href="/" class="nostyle"><h1 style="font-size: clamp(2px,8vw,55px);">Rainn.ee</h1></a>
+		</div>
+		<!--blank div for spacing -->
+		<div class="flex-item"></div>
+		<nav class="flex-item" style = "padding-right: 1vw;">	
+			<ul class="flex flex-item flex-center" style="padding:5px; justify-content: space-between;">
+			
+				<div><h1><a href="/about">About</a></h1></div>
+				<div><h1><a href="/contact">Contact</a></h1></div>
+				<div><h1><a href="/projects">Projects</a></h1></div>
+				<div><h1><a href="/resume">Resume</a></h1></div>
+			</ul>
+		</nav>
 	</header>

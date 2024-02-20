372ca31042344bafd530b36da102924639d6c81d
Ray Wofford <raywofforddesign@gmail.com>
2024-02-19 23:12:58 -0600

    testing netlify form detection

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -97,4 +97,16 @@
   </p>
 </form>
 
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
 <a href="/"> go back </a>
